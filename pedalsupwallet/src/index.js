5f26e77cbb1984fd044876cc5208fd04588d473f
Saurabh Davda <saurabhdavda9977@gmail.com>
2025-03-30 16:07:18 +0530

    updated

--- pedalsupwallet/src/index.js
+++
@@ -1,4 +0,0 @@
-export { PedalsUpProvider } from './components/PedalsUpProvider';
-export { ConnectButton } from './components/ConnectButton';
-export { AccountInfo } from './components/AccountInfo';
-export { useWallet } from './hooks/useWallet';
